eb6b3c171f753e467435b47d031d5a5ecd4a5530
ThiagoDambroski <dambroskic@hotmail.com>
2022-10-03 19:42:15 -0300

    rectangle update

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 
 push = require 'push'
 
-WINDOW_WIDTH = 1200
+WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720 
 
 VIRTUAL_WIDTH = 432
@@ -11,6 +11,11 @@ VIRTUAL_HEIGHT = 243
 function love.load()
 
     love.graphics.setDefaultFilter("nearest","nearest")
+
+    newFont = love.graphics.newFont('font.ttf',8)
+
+    love.graphics.setFont(newFont)
+
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,
     {fullscreen = false, resizable= false, vsync = true})
     
@@ -27,9 +32,19 @@ end
 function love.draw()
    
     push:apply('start')
-
     
-    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+---left
+    love.graphics.rectangle('fill',10,30,5,20)
+
+---right
+    love.graphics.rectangle("fill",VIRTUAL_WIDTH -10, VIRTUAL_HEIGHT - 50,5,20)
+
+---ball
+    love.graphics.rectangle("fill",VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT /2 - 2,4,4)
 
     push:apply('end')
 end
