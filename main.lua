a8f3e4ecb43dd99e7f62d8f3483b3780fc30cf78
ThiagoDambroski <dambroskic@hotmail.com>
2022-10-04 20:10:53 -0300

    Collision update

--- main.lua
+++ main.lua
@@ -46,6 +46,46 @@ function love.load()
 end
 
 function love.update(dt)
+     if gameState == 'play' then
+
+
+        if ball:collides(player1) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player1.x + 5
+
+            if ball.dy < 0 then
+                ball.dy = -math.random(10,150)
+            else
+                ball.dy = math.random(10,150)
+            end
+        end
+
+
+        if ball:collides(player2) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player2.x -4 
+            
+            if ball.dy < 0 then
+                ball.dy = -math.random(10,150)
+            else
+                ball.dy = math.random(10,150)
+            end
+        end
+
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = -ball.dy
+        end
+
+        if ball.y >= VIRTUAL_HEIGHT - 4 then
+            ball.y = VIRTUAL_HEIGHT - 4
+            ball.dy = -ball.dy
+        end
+
+
+     end
+
+
 --- player 1
     player1:update(dt)
 --- player 2
@@ -53,9 +93,11 @@ function love.update(dt)
 
     if gameState == 'play' then
         ball:update(dt)
+        
     end
 
 end
+
 
 function love.keypressed(key)
     
