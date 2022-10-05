a3c1f9f4152ae2a4d9e55f799682e6c1a65b68b0
ThiagoDambroski <dambroskic@hotmail.com>
2022-10-05 10:39:57 -0300

    sounds and resize

--- main.lua
+++ main.lua
@@ -30,8 +30,15 @@ function love.load()
 
     newFontBigger = love.graphics.newFont('font.ttf',32) 
 
+
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav','static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav','static')
+    }
+
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,
-    {fullscreen = false, resizable= false, vsync = true})
+    {fullscreen = false, resizable = true, vsync = true})
 
     player1Score = 0
     player2Score = 0
@@ -44,6 +51,11 @@ function love.load()
     
 
     gameState = 'start'
+    
+end
+
+function love.resize(w,h)
+    push:resize(w,h)
     
 end
 
@@ -69,6 +81,8 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+
+            sounds.paddle_hit:play()
         end
 
 
@@ -81,21 +95,26 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+
+            sounds['paddle_hit']:play()
         end
 
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds.wall_hit:play()
         end
 
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+            sounds.wall_hit:play()
         end
 
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
+            sounds.score:play()
             if player2Score == 10 then
                 winningPlayer = 2
                 gameState = 'over'
@@ -111,6 +130,7 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            sounds.score:play()
             if player1Score == 10 then
                 winningPlayer = 1
                 gameState = 'over'
